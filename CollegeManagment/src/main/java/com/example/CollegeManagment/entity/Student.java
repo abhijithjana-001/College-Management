df9ddc31ed452085886076b35ef8f24541d5123a
abhijithjana-001 <abhijith.kme19cs002@gmail.com>
2023-12-01 14:52:24 +0530

    Entity created

--- CollegeManagment/src/main/java/com/example/CollegeManagment/entity/Student.java
+++ CollegeManagment/src/main/java/com/example/CollegeManagment/entity/Student.java
@@ -1,4 +1,21 @@
 package com.example.CollegeManagment.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="student")
 public class Student {
+    @Id
+    private Long student_id;
+    private String  name;
 }
