9f55df434ed0039a9d3a3348a098568a507c28c7
abhijithjana-001 <abhijith.kme19cs002@gmail.com>
2023-12-01 17:01:18 +0530

    studentdto

--- CollegeManagment/src/main/java/com/example/CollegeManagment/dto/requestdto/studentdto.java
+++ CollegeManagment/src/main/java/com/example/CollegeManagment/dto/requestdto/studentdto.java
@@ -1,4 +1,17 @@
 package com.example.CollegeManagment.dto.requestdto;
 
+import com.example.CollegeManagment.entity.Department;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class studentdto {
+    private String  name;
+    private Department department;
 }
