2b62466464309fd36797f4f2a954b27a025979c1
deepak <deepakshaji83@gmail.com>
2023-12-04 17:10:27 +0530

    teacheradding,updating,listing,deleting implemented

--- CollegeManagment/src/main/java/com/example/CollegeManagment/controller/TeacherController.java
+++ CollegeManagment/src/main/java/com/example/CollegeManagment/controller/TeacherController.java
@@ -6,10 +6,9 @@ import com.example.CollegeManagment.entity.Teacher;
 import com.example.CollegeManagment.service.impl.TeacherServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "teacher")
@@ -20,6 +19,25 @@ public class TeacherController {
     @PostMapping("/addTeacher")
     public ResponseEntity<Responsedto<Teacher>> addTeacher(@RequestBody TeacherRequestDTO teacherRequestDTO) {
         Responsedto<Teacher> Responsedto=teacherServiceImpl.addTeacher(teacherRequestDTO);
+        return ResponseEntity.ok(Responsedto);
+    }
+
+    @GetMapping("/teachers")
+    public ResponseEntity<Responsedto<List<Teacher>>> findAll(){
+        Responsedto<List<Teacher>> all=teacherServiceImpl.findAll();
+        return ResponseEntity.ok(all);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Responsedto<Teacher>> update(@RequestBody TeacherRequestDTO teacherRequestDTO,
+                                                                @PathVariable long id) {
+        Responsedto<Teacher> Responsedto = teacherServiceImpl.update(id, teacherRequestDTO);
+        return ResponseEntity.ok(Responsedto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Responsedto<Teacher>> delete(@PathVariable long id) {
+        Responsedto<Teacher> Responsedto = teacherServiceImpl.delete(id);
         return ResponseEntity.ok(Responsedto);
     }
 
