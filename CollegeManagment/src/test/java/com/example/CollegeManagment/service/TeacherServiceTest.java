afc9c7a09316698b3de1b524c3392a08ac3523a4
deepak <deepakshaji83@gmail.com>
2023-12-06 09:51:49 +0530

    Teachers testController

--- CollegeManagment/src/test/java/com/example/CollegeManagment/service/TeacherServiceTest.java
+++ CollegeManagment/src/test/java/com/example/CollegeManagment/service/TeacherServiceTest.java
@@ -1,118 +1,121 @@
 package com.example.CollegeManagment.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import com.example.CollegeManagment.Exception.ItemNotFound;
 import com.example.CollegeManagment.dto.requestdto.TeacherRequestDTO;
 import com.example.CollegeManagment.dto.responsedto.Responsedto;
+import com.example.CollegeManagment.entity.Department;
 import com.example.CollegeManagment.entity.Teacher;
 import com.example.CollegeManagment.repository.DepartmentRepo;
 import com.example.CollegeManagment.repository.TeacherRepo;
 import com.example.CollegeManagment.service.impl.TeacherServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.*;
+@ExtendWith(MockitoExtension.class)
+public class TeacherServiceTest {
 
-    @ExtendWith(MockitoExtension.class)
-    public class TeacherServiceTest {
+    @Mock
+    private TeacherRepo teacherRepo;
 
+    @Mock
+    private DepartmentRepo departmentRepo;
 
-        @Mock
-        private TeacherRepo teacherRepo;
+    @InjectMocks
+    private TeacherServiceImpl teacherService;
 
-        @Mock
-        private DepartmentRepo departmentRepo;
-
-        @InjectMocks
-        private TeacherServiceImpl teacherService;
-
-        @Test
-        void TeacherServiceTest_addTeacher() {
-            Teacher teacher = new Teacher();
-            teacher.setName("John");
-            teacher.setDepartments(new HashSet<>());
-
-            TeacherRequestDTO teacherRequestDTO = new TeacherRequestDTO();
-            teacherRequestDTO.setName("John");
-            teacherRequestDTO.setDepartment(new HashSet<>());
-
-            when(teacherRepo.save(Mockito.any(Teacher.class))).thenReturn(teacher);
-
-            Responsedto<Teacher> teacherResponseDTO = teacherService.addTeacher(teacherRequestDTO);
-
-            verify(teacherRepo, times(1)).save(Mockito.any(Teacher.class));
-
-            assertAll(() -> {
-                assertThat(Responsedto).isNotNull();
-                assertThat(Responsedto.getResult()).isEqualTo(teacher);
-            });
-        }
-
-        @Test
-        void TeacherServiceTest_findAll() {
-            Teacher teacher = new Teacher();
-            teacher.setName("John");
-            teacher.setDepartments(new HashSet<>());
-
-            List<Teacher> teacherList = Arrays.asList(teacher, teacher);
-
-            when(teacherRepo.findAll()).thenReturn(teacherList);
-
-            Responsedto<List<Teacher>> teacherResponseDTO = teacherService.findAll();
-
-            verify(teacherRepo, times(1)).findAll();
-
-            assertAll(() -> {
-                assertThat(teacherResponseDTO).isNotNull();
-                assertThat(teacherResponseDTO.getResult()).hasSize(2);
-            });
-        }
-
-        @Test
-        void TeacherServiceTest_update() {
-            Long id = 1L;
-            Teacher teacher = new Teacher();
-            teacher.setId(id);
-            teacher.setName("John");
-            teacher.setDepartments(new HashSet<>());
-
-            TeacherRequestDTO teacherRequestDTO = new TeacherRequestDTO();
-            teacherRequestDTO.setName("Jane");
-            teacherRequestDTO.setDepartment(new HashSet<>());
-
-            when(teacherRepo.findById(Mockito.any(Long.class))).thenReturn(Optional.ofNullable(teacher));
-            when(teacherRepo.save(Mockito.any(Teacher.class))).thenReturn(teacher);
-
-            Responsedto<Teacher> teacherResponseDTO = teacherService.update(id, teacherRequestDTO);
-
-            verify(teacherRepo, times(1)).findById(Mockito.any(Long.class));
-            verify(teacherRepo, times(1)).save(Mockito.any(Teacher.class));
-
-            assertAll(() -> {
-                assertThat(teacherResponseDTO).isNotNull();
-                assertThat(teacherResponseDTO.getResult()).isEqualTo(teacher);
-            });
-        }
-
-        @Test
-        void TeacherServiceTest_delete() {
-            Long id = 1L;
-            doNothing().when(teacherRepo).deleteById(id);
-            teacherService.delete(id);
-            verify(teacherRepo, times(1)).deleteById(id);
-        }
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
     }
 
+    @Test
+    public void testAddTeacher() {
+        TeacherRequestDTO teacherRequestDTO = new TeacherRequestDTO();
+        teacherRequestDTO.setName("Test Teacher");
+        Department department = new Department();
+        department.setId(1L);
+        department.setName("Test Department");
+        teacherRequestDTO.setDepartment((Set<Department>) department);
 
+        Teacher savedTeacher = new Teacher();
+        savedTeacher.setTid(1L);
+        savedTeacher.setName("Test Teacher");
+        savedTeacher.setDepartments((Set<Department>) department);
 
+        when(departmentRepo.findById(1L)).thenReturn(Optional.of(department));
+        when(teacherRepo.save(any(Teacher.class))).thenReturn(savedTeacher);
 
+        Responsedto<Teacher> response = teacherService.addTeacher(teacherRequestDTO);
+
+        assertTrue(response.getSuccess());
+        assertEquals("Added Successfully", response.getMessage());
+        assertEquals(savedTeacher, response.getResult());
+    }
+
+    @Test
+    public void testFindAllTeachers() {
+        List<Teacher> teachers = new ArrayList<>();
+        Teacher teacher1 = new Teacher();
+        teacher1.setTid(1L);
+        teacher1.setName("Teacher 1");
+        teachers.add(teacher1);
+
+        Teacher teacher2 = new Teacher();
+        teacher2.setTid(2L);
+        teacher2.setName("Teacher 2");
+        teachers.add(teacher2);
+
+        when(teacherRepo.findAll()).thenReturn(teachers);
+
+        Responsedto<List<Teacher>> response = teacherService.findAll();
+
+        assertTrue(response.getSuccess());
+        assertEquals("Teachers List", response.getMessage());
+        assertEquals(teachers, response.getResult());
+    }
+
+    @Test
+    public void testUpdateTeacher() {
+        long teacherId = 1L;
+        TeacherRequestDTO teacherRequestDTO = new TeacherRequestDTO();
+        teacherRequestDTO.setName("Updated Teacher");
+        Department department = new Department();
+        department.setId(1L);
+        department.setName("Updated Department");
+        teacherRequestDTO.setDepartment((Set<Department>) department);
+
+        Teacher existingTeacher = new Teacher();
+        existingTeacher.setTid(teacherId);
+        existingTeacher.setName("Old Teacher");
+        existingTeacher.setDepartments(new HashSet<>());
+
+        when(teacherRepo.findById(teacherId)).thenReturn(Optional.of(existingTeacher));
+        when(teacherRepo.save(any(Teacher.class))).thenReturn(existingTeacher);
+
+        Responsedto<Teacher> response = teacherService.update(teacherId, teacherRequestDTO);
+
+        assertTrue(response.getSuccess());
+        assertEquals("Updated Successfully", response.getMessage());
+        assertEquals(existingTeacher, response.getResult());
+    }
+
+    @Test
+    public void testDeleteTeacher() {
+        long teacherId = 1L;
+
+        Responsedto<Teacher> response = teacherService.delete(teacherId);
+
+        assertNull(response.getResult());
+        assertTrue(response.getSuccess()); // Assuming success even if data is null
+    }
+}
