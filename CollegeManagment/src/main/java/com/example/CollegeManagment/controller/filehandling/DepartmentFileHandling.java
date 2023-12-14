d4e699575e775926d03a71ce66b72e6a4812e7cf
saurav <saurav.devarajan@piserve.com>
2023-12-14 12:57:26 +0530

    updated department file handling

--- CollegeManagment/src/main/java/com/example/CollegeManagment/controller/filehandling/DepartmentFileHandling.java
+++ CollegeManagment/src/main/java/com/example/CollegeManagment/controller/filehandling/DepartmentFileHandling.java
@@ -1,5 +1,6 @@
 package com.example.CollegeManagment.controller.filehandling;
 
+import com.example.CollegeManagment.dto.responsedto.Responsedto;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -7,20 +8,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/department/file")
 public class DepartmentFileHandling {
-    private final String FILE_DIRECTORY = "C://Users//user432//Desktop//fileUpload//";
+    private final String uploadDir = "C://Users//user432//Desktop//fileUpload//";
 
     @PostMapping("/upload")
     public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
         try {
 
-            Path filePath = Paths.get(FILE_DIRECTORY, file.getOriginalFilename());
+            Path filePath = Paths.get(uploadDir, file.getOriginalFilename());
             file.transferTo(filePath.toFile());
             return ResponseEntity.ok("File uploaded successfully!");
         } catch (IOException e) {
@@ -29,27 +32,27 @@ public class DepartmentFileHandling {
         }
     }
 
-    @GetMapping("/download/{fileName}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) throws IOException {
-        // Load file as Resource
-        Path filePath = Paths.get(FILE_DIRECTORY).resolve(fileName).normalize();
-        Resource resource = new org.springframework.core.io.FileUrlResource(String.valueOf(filePath.toUri()));
-
-        // Check if the file exists
-        if (!resource.exists()) {
-            return ResponseEntity.notFound().build();
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<byte[]> getfile(@PathVariable String filename) throws IOException {
+        Path path=Paths.get(uploadDir,filename);
+        String contentType = Files.probeContentType(path);
+        if (contentType == null) {
+            contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
         }
+        byte[] image= Files.readAllBytes(path);
+        return ResponseEntity.status(200).contentType(MediaType.valueOf(contentType)).body(image);
+    }
 
-        // Set Content-Disposition header for the response
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename());
+    @DeleteMapping("/delete/{filename}")
+    public  ResponseEntity<Responsedto> handleFileDelete(@PathVariable String filename)
+    {
+        File file=new File(uploadDir,filename);
+        if(file.exists() && file.delete()){
 
-        // Set the Content-Type header based on the file type
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-
-        // Return the ResponseEntity
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(resource);
+            return ResponseEntity.ok(new Responsedto<>(true,"File delete successfully!",null));
+        }
+        else{
+            return  ResponseEntity.status(404).body(new Responsedto<>(false,"File delete successfully!",null));
+        }
     }
 }
